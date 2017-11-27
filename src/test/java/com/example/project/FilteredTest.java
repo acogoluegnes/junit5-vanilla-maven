f631bcbaf4dc87673692edcce647e0111f8a1829
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-11-27 11:45:32 +0100

    Add to-be-excluded test

---
+++ src/test/java/com/example/project/FilteredTest.java
@@ -0,0 +1,17 @@
+package com.example.project;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+@Tag("filtered")
+public class FilteredTest {
+
+    @Test
+    public void dummy() {
+
+    }
+
+}
